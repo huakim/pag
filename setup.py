cbdcc671ccea8aa69ec6f2db46e3f0134211bfef
Ralph Bean <rbean@redhat.com>
2016-04-11 09:37:25 -0400

    Fix license classifier.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url='https://pagure.io/pag',
     license='GPLv3+',
     classifiers=[
-        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Programming Language :: Python :: 3 :: Only",
     ],
     install_requires=requires,
