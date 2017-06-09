6db02a3089e8b23f9cd1b59742328d7f88f129cb
Lubomír Sedlář <lsedlar@redhat.com>
2017-06-09 13:17:14 +0200

    Dim output of subprocess
    If colorama package is available, dim the output of invoked commands.

--- pag/utils.py
+++ pag/utils.py
@@ -7,6 +7,13 @@ import click
 import requests
 import yaml
 
+try:
+    from colorama import Style
+    DIM = Style.DIM
+    RESET = Style.RESET_ALL
+except ImportError:
+    DIM = RESET = ''
+
 
 CONF_FILE = os.path.expanduser('~/.config/pag')
 
@@ -17,7 +24,7 @@ def run(cmd, echo=True, graceful=True):
     output, _ = proc.communicate()
     output = output.decode('utf-8')
     if echo:
-        click.echo(output)
+        click.echo(DIM + output + RESET)
     if not graceful and proc.returncode != 0:
         sys.exit(1)
     return proc.returncode, output
