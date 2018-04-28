d5b39ee878da8f9c528acf88b2baa736bfbb8b1b
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2018-04-28 22:07:13 +0200

    Reduce code duplication in pag.client

--- pag/client.py
+++ pag/client.py
@@ -32,29 +32,36 @@ class Pagure(fedora.client.OpenIdBaseClient):
         response = self._session.get(self.base_url)
         return "logout" in response.text
 
-    def create(self, name, description):
-        if not self.is_logged_in:
-            raise PagureException('Not logged in.')
-        url = self.base_url + '/new'
+    def _get_csrf_token(self, url):
         response = self._session.get(url)
         if not bool(response):
             raise PagureException("Couldn't get form to get "
                                   "csrf token %r" % response)
 
         soup = bs4.BeautifulSoup(response.text, "html.parser")
-        data = dict(
-            csrf_token=soup.find(id='csrf_token').attrs['value'],
-            name=name,
-            description=description,
-        )
+        return soup.find(id='csrf_token').attrs['value']
 
+    def _post(self, url, data, action=''):
         response = self._session.post(url, data=data)
 
         if not bool(response):
             del data['csrf_token']
             raise PagureException('Bad status code from pagure when '
-                                  'creating project: %r.  Sent %r' % (
-                                      response, data))
+                                  '%s: %r.  Sent %r' % (
+                                      action, response, data))
+        return response
+
+    def create(self, name, description):
+        if not self.is_logged_in:
+            raise PagureException('Not logged in.')
+        url = self.base_url + '/new'
+        data = dict(
+            csrf_token=self._get_csrf_token(url),
+            name=name,
+            description=description,
+        )
+
+        self._post(url, data=data, action='creating project')
         return repo_url(name)
 
     def create_issue(self, repo, title, description, private=False):
@@ -62,25 +69,14 @@ class Pagure(fedora.client.OpenIdBaseClient):
             raise PagureException('Not logged in.')
 
         url = self.base_url + '/' + repo + '/new_issue'
-        response = self._session.get(url)
-        if not bool(response):
-            raise PagureException("Couldn't get form to get "
-                                  "csrf token %r" % response)
-
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
         data = {
-            'csrf_token': soup.find(id='csrf_token').attrs['value'],
+            'csrf_token': self._get_csrf_token(url),
             'title': title,
             'issue_content': description,
             'private': private
         }
 
-        response = self._session.post(url, data=data)
-        if not bool(response):
-            del data['csrf_token']
-            raise PagureException('Bad status code from pagure when '
-                                  'forking project: %r.  Sent %r' % (
-                                      response, data))
+        response = self._post(url, data=data, action='creating issue')
 
         soup = bs4.BeautifulSoup(response.text, "html.parser")
         response_title = soup.title.string
@@ -94,14 +90,8 @@ class Pagure(fedora.client.OpenIdBaseClient):
             raise PagureException('Not logged in.')
 
         url = self.base_url + '/' + repo + '/upload'
-        response = self._session.get(url)
-        if not bool(response):
-            raise PagureException("Couldn't get form to get "
-                                  "csrf token %r" % response)
-
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
         data = {
-            'csrf_token': soup.find(id='csrf_token').attrs['value'],
+            'csrf_token': self._get_csrf_token(url),
         }
         files = {
             'filestream': open(filepath, 'rb')
@@ -123,50 +113,25 @@ class Pagure(fedora.client.OpenIdBaseClient):
             raise PagureException('Not logged in.')
 
         url = self.base_url + '/' + name
-        response = self._session.get(url)
-        if not bool(response):
-            raise PagureException("Couldn't get form to get "
-                                  "csrf token %r" % response)
-
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
         data = dict(
-            csrf_token=soup.find(id='csrf_token').attrs['value'],
+            csrf_token=self._get_csrf_token(url),
         )
 
         url = self.base_url + '/do_fork/' + name
-        response = self._session.post(url, data=data)
-
-        if not bool(response):
-            del data['csrf_token']
-            raise PagureException('Bad status code from pagure when '
-                                  'forking project: %r.  Sent %r' % (
-                                      response, data))
+        self._post(url, data=data, action='forking project')
         return repo_url(name)
 
     def submit_pull_request(self, name, base, head, title, comment):
         url = self.base_url + '/{name}/diff/{base}..{head}'
         url = url.format(name=name, base=base, head=head)
 
-        response = self._session.get(url)
-        if not bool(response):
-            raise PagureException("Couldn't get form to get "
-                                  "csrf token %r" % response)
-
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
         data = dict(
-            csrf_token=soup.find(id='csrf_token').attrs['value'],
+            csrf_token=self._get_csrf_token(url),
             branch_to=base,
             title=title,
             initial_comment=comment,
         )
-        response = self._session.post(url, data=data)
-
-        if not bool(response):
-            del data['csrf_token']
-            raise PagureException('Bad status code from pagure when '
-                                  'creating pull request: %r.  Sent %r' % (
-                                      response, data))
-
+        response = self._post(url, data=data, action='creating pull request')
         return response.url
 
 
